b93376445f0b15b5cb195e5c3e4088fe28af85c6
Lubov Fedorova <luba.zaverugo@gmail.com>
2021-03-06 15:51:26 +0300

    cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  let arr = [];
+      for (let cat of backyard) {
+        let result = cat.filter(item => item === '^^' ); 
+        arr = arr.concat(result);
+      }
+      return arr.length;
 };
