a2760610d4b8661ebd538f30648181846d9d9067
Lubov Fedorova <luba.zaverugo@gmail.com>
2021-03-06 17:03:07 +0300

    team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    let arr =[];
+    for (let member of members) {
+      if (typeof member === 'string') {
+        let r = member.trim().split('');
+        let upperR = r[0].toUpperCase();
+        arr.push(upperR);
+      } else {
+        continue;
+      }
+    }
+    function compare(a,b) {
+          if(a>b) return 1;
+          if (a==b) return 0;
+          if (a<b) return -1;
+        }
+    return arr.sort(compare).join('');
+  } else {
+    return false
+  }
 };
